137a3611a06e418e3b3ad419ef1738b3d1637320
JeanWard005 <jeanward005@gmail.com>
2022-05-18 00:40:50 -0400

    Updated MyFirstScript And VolumeController Script
    updated muffins click button text box counter script

--- Assets/MuffinClicker/Scripts/MyFirstScript.cs
+++ Assets/MuffinClicker/Scripts/MyFirstScript.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MyFirstScript : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI _totalMuffinsText;
    public int m_counter = 0;
 
   public void OnMuffinClicked()
     {
         m_counter = m_counter + 1;
-
+        _totalMuffinsText.text =  m_counter.ToString() + " Muffins" ;
         Debug.Log( m_counter );
 
     }
--- Assets/MuffinClicker/Scripts/VolumeController.cs
+++ Assets/MuffinClicker/Scripts/VolumeController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
 {
+
     public void OnValueChanged()
     {
+
+
         Debug.Log("Value has been changed ");
 
     }
